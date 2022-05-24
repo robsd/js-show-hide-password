31cf1bd72f6ad6a4c31bfb578ae3cdeac97f968e
Robert Dixon <79385125+robsd@users.noreply.github.com>
2022-05-25 00:18:34 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# js-show-hide-password 👁
+
+A method of showing and hiding a password field in JavaScript.
+
+## Demo
+
+https://robsd.github.io/js-show-hide-password
---
+++ js/script.js
@@ -0,0 +1,13 @@
+password = document.getElementById('password');
+showHidePassword = document.getElementById('show-hide-password');
+
+showHidePassword.onclick = function() {
+	if (password.type == "password") {
+		password.type = "text";
+		showHidePassword.innerHTML = '<i class="fas fa-eye"></i>';
+	}
+	else {
+		password.type = "password";
+		showHidePassword.innerHTML = '<i class="fas fa-eye-slash"></i>';
+	}
+}
